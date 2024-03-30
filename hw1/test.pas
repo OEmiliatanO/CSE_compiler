937e969b89fbd7b7082e2262dbe180888925d7bb
OEmiliatanO <emiliaistruelove@gmail.com>
2024-03-31 06:12:28 +0800

    update

--- hw1/test.pas
+++ hw1/test.pas
@@ -1,6 +1,9 @@
 program test;
 var
+  i32: longword;
+  i64: cardinal;
   i : integer;
+  f : real;
   s : string;
 begin
   (* this is 
@@ -9,11 +12,17 @@ begin
   (******)
   (*
      *)
-  
+  *)
   read(i);
   s := ab';
   s := 'ab;
   s := 'hello []]1''''''';
   s := 'ab'a';
   s := ''a''
+  i := i + 001;
+  i := i + 0100;
+  f := f + 0.1;
+  f := f + .1;
+  f := f + .0001;
+  f := f + 1.;
 end;
